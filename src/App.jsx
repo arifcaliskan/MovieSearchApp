d36e18ba068c6edd2271fc4060da538a3290e3ec
ArifCaliskan <arif.caliskan@agu.edu.tr>
2023-07-12 11:26:54 +0300

    First commit
    First commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search-icon.svg";
+import MovieCard from "./MovieCard";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Spider");
+  }, []);
+  return (
+    <div className="app">
+      <h1>Movie App </h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+
+        <img
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => {
+            searchMovies(searchTerm);
+          }}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+export default App;
+
+// c117959a
+const API_URL = "http://www.omdbapi.com?apikey=c117959a";
+const movie = {
+  Title: "Spider-Man: No Way Home",
+  Year: "2021",
+  imdbID: "tt10872600",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_SX300.jpg",
+};
